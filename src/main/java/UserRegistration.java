8a904cf96b46408a21b70c0295109bd969ac2583
Shubham <ssp671995@gmail.com>
2022-04-03 13:11:17 +0530

    Added valid first name using test cases.

--- src/main/java/UserRegistration.java
+++ src/main/java/UserRegistration.java
@@ -1,2 +1,27 @@
+/**
+ * Program on User Registration Using Regex
+ *
+ * @author : Shubham Pawar
+ * @since : 01/04/2022
+ */
+
+/**
+ * created class name as UserRegistration
+ */
 public class UserRegistration {
+    /**
+     * created method checkFName and passing parameter in this it will check
+     * FirstName is valid or not result will be in true or false because method is
+     * boolean type
+     *
+     * @param fName -FirstName of the String
+     * @return -return to method created
+     */
+    public boolean checkFName(String fName) {
+        /**
+         * regex pattern for FirstName
+         */
+        return (fName.matches("[A-Z][a-z]{3,}"));
+
+    }
 }
--- src/test/java/UserRegTest.java
+++ src/test/java/UserRegTest.java
@@ -1,2 +1,34 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * testCases for validating firstName.
+ */
+
 public class UserRegTest {
+    @Test
+    /**
+     * created method testingFirstName() to check firstname is valid or not
+     */
+    public void testingFirstName() {
+        /**
+         * Creating Object for UserRegistration class
+         */
+        UserRegistration userRegistration = new UserRegistration();
+        /**
+         * calling checkFName method to see firstname is valid
+         */
+        boolean isFNameValid = userRegistration.checkFName("Almas");
+        Assert.assertTrue(isFNameValid);
+    }
+
+    @Test
+    /**
+     * created method testFirstNameInvalidMustReturnFalse for false condition
+     */
+    public void testFirstNameInvalidMustReturnFalse() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean isFnameValid = userRegistration.checkFName("almas");
+        Assert.assertFalse(isFnameValid);
+    }
 }
